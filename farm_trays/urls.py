e0640e179348cd8e15d01fbfd59960379646a015
Lorenz Flores <138431499+Uriah08@users.noreply.github.com>
2025-11-10 10:49:01 +0800

    Rename and Delete Farm Tray

--- farm_trays/urls.py
+++ farm_trays/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('create/', views.CreateFarmTrayView.as_view(), name="create-farm-tray"),
     path('get/<int:farm_id>/', views.GetFarmTraysView.as_view(), name="get-farm-trays"),
     path('get/tray/<int:tray_id>/', views.GetFarmTrayByIdView.as_view(), name="get-farm-tray"),
+    path('maintenance/<int:tray_id>/', views.TrayMaintenanceView.as_view(), name="tray-maintenance"),
+    path('rename/<int:tray_id>/', views.RenameTrayView.as_view(), name="rename-tray"),
+    path('delete/<int:tray_id>/', views.DeleteTrayView.as_view(), name="delete-tray"),
 ]
